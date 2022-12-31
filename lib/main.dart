8cce01748b04d4c5fe92da42eb7035e6e56effc4
Yu Muramatsu <me@murayu.com>
2023-01-01 00:50:58 +0900

    feat&refactor: Create welcome page! and file organization

--- lib/main.dart
+++ lib/main.dart
@@ -1,150 +1,43 @@
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
-import 'package:crypto/crypto.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:pictapp/pages/welcome.dart';
+import 'package:pictapp/pages/signin.dart';
+import 'package:pictapp/pages/signup.dart';
 
 Future<void> main() async {
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+  runApp(const PICTApp());
 }
 
-class MyApp extends StatelessWidget {
+class PICTApp extends StatelessWidget {
+  const PICTApp({super.key});
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'PICT Login App',
+      supportedLocales: const [Locale('ja', 'JP')],
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        DefaultCupertinoLocalizations.delegate
+      ],
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyAuthPage(),
-    );
-  }
-}
-
-class MyAuthPage extends StatefulWidget {
-  @override
-  _MyAuthPageState createState() => _MyAuthPageState();
-}
-
-class _MyAuthPageState extends State<MyAuthPage> {
-  // 入力されたメールアドレス
-  String newUserName = "";
-  // 入力されたパスワード
-  String newUserPassword = "";
-  // 入力されたニックネーム
-  String newNickName = "";
-  // 入力されたメールアドレス（ログイン）
-  String loginUserName = "";
-  // 入力されたパスワード（ログイン）
-  String loginUserPassword = "";
-  // 登録・ログインに関する情報を表示
-  String infoText = "";
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Container(
-          padding: EdgeInsets.all(40),
-          child: Column(
-            children: <Widget>[
-              const SizedBox(height: 30),
-              TextFormField(
-                // テキスト入力のラベルを設定
-                decoration: InputDecoration(labelText: "ユーザー名（英数字のみ）"),
-                onChanged: (String value) {
-                  setState(() {
-                    newUserName = value;
-                  });
-                },
-              ),
-              const SizedBox(height: 8),
-              TextFormField(
-                // テキスト入力のラベルを設定
-                decoration: InputDecoration(labelText: "ニックネーム（英数字のみ）"),
-                onChanged: (String value) {
-                  setState(() {
-                    newNickName = value;
-                  });
-                },
-              ),
-              const SizedBox(height: 8),
-              TextFormField(
-                decoration: InputDecoration(labelText: "パスワード（英数字のみ）"),
-                // パスワードが見えないようにする
-                obscureText: true,
-                onChanged: (String value) {
-                  setState(() {
-                    newUserPassword = value;
-                  });
-                },
-              ),
-              const SizedBox(height: 8),
-              ElevatedButton(
-                onPressed: () async {
-                  final res = await http.get(Uri.parse('http://127.0.0.1:5000/signup?u=$newUserName'));
-
-                  if(res.statusCode == 200) {
-                    var jsonData = jsonDecode(res.body);
-                    if(jsonData['status'] == 200) {
-                      var salt = jsonData['salt'];
-                      var saltedPassword = salt + newUserPassword;
-                      var hashedPassword = sha256.convert(utf8.encode(saltedPassword)).toString();
-                      final res2 = await http.get(Uri.parse('http://127.0.0.1:5000/signup/auth?u=$newUserName&p=$hashedPassword&n=$newNickName'));
-                      if (res2.statusCode == 200) {
-                        var jsonData2 = jsonDecode(res2.body);
-                        if(jsonData2['status'] == 200) {
-                          setState(() {
-                            infoText = "登録に成功しました";
-                          });
-                        } else {
-                          setState(() {
-                            infoText = "登録に失敗しました";
-                          });
-                        }
-                      } else {
-                        setState(() {
-                          infoText = "登録に失敗しました";
-                        });
-                      }
-                    }
-                  } else {
-                    setState(() {
-                      infoText = "登録に失敗しました";
-                    });
-                  }
-                },
-                child: Text("ユーザー登録"),
-              ),
-              const SizedBox(height: 32),
-              TextFormField(
-                decoration: InputDecoration(labelText: "ユーザー名"),
-                onChanged: (String value) {
-                  setState(() {
-                    loginUserName = value;
-                  });
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: "パスワード"),
-                obscureText: true,
-                onChanged: (String value) {
-                  setState(() {
-                    loginUserPassword = value;
-                  });
-                },
-              ),
-              const SizedBox(height: 8),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text("ログイン"),
-              ),
-              const SizedBox(height: 8),
-              Text(infoText),
-            ],
-          ),
-        ),
-      ),
+      initialRoute: 'welcome',
+      routes: {
+        'welcome': (context) => const WelcomePage(),
+        'signin': (context) => const SignInPage(),
+        'signup': (context) => const SignUpPage(),
+      },
     );
   }
 }
---
+++ lib/pages/signin.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/material.dart';
+import 'package:pictapp/pages/signup.dart';
+
+class SignInPage extends StatefulWidget {
+  const SignInPage({super.key});
+
+  @override
+  State<SignInPage> createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage>{
+  double? _deviceHeight, _deviceWidth;
+  String _username = '';
+  String _usernickname = '';
+  String _password = '';
+  String _repassword = '';
+  bool isVisible = false;
+
+  final _formKey = GlobalKey<FormState>();
+
+  void toggleShowPassword () {
+    setState(() {
+      isVisible = !isVisible;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: _deviceWidth! * 0.05),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  _title(),
+                  _signupForm(),
+                  _alreadyHaveAccount(),
+                ],
+              ),
+            ),
+          ),
+        )
+    );
+  }
+  Widget _title() {
+    return Column(
+      children: [
+        Text(
+          'お帰りなさい！',
+          style: TextStyle(
+            fontSize: _deviceWidth! * 0.1,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          'ログインしましょう',
+          style: TextStyle(
+            fontSize: _deviceWidth! * 0.05,
+            fontWeight: FontWeight.w300,
+          ),
+        ),
+      ],
+    );
+  }
+  Widget _signupForm() {
+    return Form(
+      key: _formKey,
+      child: Column(
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'ユーザー名',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return '必須項目です';
+                } else if (value.length < 6) {
+                  return '6文字以上です';
+                } else if (value.length > 16) {
+                  return '20文字以下です';
+                } else if (value.contains(' ')) {
+                  return 'スペースは使用できません';
+                } else if (value.contains(RegExp(r'<>[-/:-[-`{-~]'))) {
+                  return '記号は@と!のみ使用できます';
+                } else if (!RegExp(r'^[A-Za-z0-9!@]*$').hasMatch(value)) {
+                  return '英語・数字・!・@のみ使用できます';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                  suffixIcon: IconButton(
+                    icon: Icon(isVisible? Icons.visibility : Icons.visibility_off),
+                    onPressed: () {
+                      toggleShowPassword();
+                    },
+                  ),
+                  labelText: "パスワード"
+              ),
+              obscureText: !isVisible,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return '必須項目です';
+                } else if (value.length < 8) {
+                  return 'パスワードは8文字以上です';
+                } else if (value.length > 16) {
+                  return 'パスワードは16文字以下です';
+                } else if (value.contains(' ')) {
+                  return 'スペースは使用できません';
+                } else if (!RegExp(r'^[A-Za-z0-9!@]*$').hasMatch(value)) {
+                  return '英語・数字・!・@のみ使用できます';
+                }
+                return null;
+              },
+              onChanged: (value) {
+                setState(() {
+                  _password = value;
+                });
+              },
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                textStyle: const TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.w600,
+                ),
+                primary: const Color(0xff09B58E),
+                onPrimary: Colors.white,
+                elevation: 10,
+                padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('データ確認作業開始')),
+                  );
+                }
+              },
+              child: const Text('ログイン'),
+            ),
+          ]),
+    );
+  }
+  Widget _alreadyHaveAccount () {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        const Text(
+          "アカウントをお持ちでない方は",
+          style: TextStyle(color: Colors.black),
+        ),
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) {
+                  return const SignUpPage();
+                },
+              ),
+            );
+          },
+          child: const Text(
+            "新規登録",
+            style: TextStyle(
+              color: Colors.blue,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
